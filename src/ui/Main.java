1accb36859bd3b5aa9d4e4812a81962edf1009a9
Alexander <alexssjr2002@gmail.com>
2021-02-24 10:14:44 -0500

    Removed CLI

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -18,7 +18,6 @@ public class Main {
 	
 	public static void readData () {
 		
-		System.out.println("Insert the text input");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		try {
